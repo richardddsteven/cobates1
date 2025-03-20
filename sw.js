6a469e4d1466af80efc2f87a80ab43d818220cd4
richarddstevenn <131933833+richarddstevenn@users.noreply.github.com>
2025-03-20 20:40:10 +0700

    hapus offline

--- sw.js
+++ sw.js
@@ -1,28 +1,22 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('first-app')
+      .then(function(cache) {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/css/app.css',
+          '/src/js/app.js'
+        ])
+      })
+  );
+  return self.clients.claim();
+});
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());
-                  return res;
-                })
-            })
-            .catch(function(err) {
-              return caches.open(CACHE_STATIC_NAME)
-                .then(function(cache) {
-                  return cache.match('/offline.html');
-                });
-            });
-        }
+      .then(function(res) {
+        return res;
       })
   );
 });
-
-  
-    
